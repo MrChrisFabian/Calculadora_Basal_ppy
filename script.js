f0bffe418f3a717da0b607079e79a0b37af7d024
MrChrisFabian <christopher.mendoza@fiuni.edu.py>
2023-12-07 14:31:03 -0300

    addede js

---
+++ script.js
@@ -0,0 +1,8 @@
+const CALCULAR = document.getElementById('calcular');
+const ERROR = document.getElementById('error');
+const FLU = document.getElementById('flu');
+const MAN = document.getElementById('man');
+
+CALCULAR.addEventListener('click', () => {
+    console.log('Se hizo click!')
+})
