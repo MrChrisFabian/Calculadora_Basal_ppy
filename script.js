19d2810f2b99f525ccbd0a72e18ae8c006955ada
MrChrisFabian <christopher.mendoza@fiuni.edu.py>
2023-12-07 17:02:55 -0300

    js fixed

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const CALCULAR = document.getElementById('calcular');
-const ERROR = document.getElementById('error');
-const FLU = document.getElementById('flu');
-const MAN = document.getElementById('man');
+const CALCULAR = document.getElementById('calcular')
+const ERROR = document.getElementById('error')
+const FLUJO = document.getElementById('flu')
+const MANTENIMIENTO = document.getElementById('man')
 
 CALCULAR.addEventListener('click', () => {
-    console.log('Se hizo click!')
+    const DATO = document.getElementById('peso').value
+    console.log("Dato Ingresado: " + DATO)
+    if (DATO > 0) {
+        ERROR.style.display = 'none'
+        let flujo = 10
+        let mantenimiento = flujo * 1.5
+        FLUJO.innerHTML = flujo + 'cc/hr'
+        MANTENIMIENTO.innerHTML = 'm+m/2' + mantenimiento + 'cc/hr'
+        FLUJO.style.display = 'block'
+        MANTENIMIENTO.style.display = 'block'
+    }
+    else {
+        ERROR.style.display = 'block'
+        FLUJO.style.display = 'none'
+        MANTENIMIENTO.style.display = 'none'
+    }
 })
+function calcFlujo(param1) {
+
+}
